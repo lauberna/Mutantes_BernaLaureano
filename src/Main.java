fe6eac8d3e1c501dba6ccef15401aebdf2f804ea
lauberna <laureanoberna2002@gmail.com>
2023-11-01 21:08:08 -0300

    corrigiendo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static boolean isMutant(String[] dna) {
         int filas = dna.length;
@@ -53,7 +55,7 @@ public class Main {
         //busca en la diagonal SECUNDARIA
         conteoActual = 1;
         for (int j = 1; j < filas; j++) {
-            if (dna[j].charAt(dna[j].length()-(1+j)) == dna[j - 1].charAt(dna[j].length()-j)) {
+            if (dna[j].charAt(dna[j].length() - (1 + j)) == dna[j - 1].charAt(dna[j].length() - j)) {
                 conteoActual++;
                 if (conteoActual == 4) {
                     contadorSecuencias++;
@@ -68,14 +70,44 @@ public class Main {
         return contadorSecuencias >= 2;
     }
 
+
     public static void main(String[] args) {
-        String[] dnaMutant = {
+        /*COMENTAR ESTE, Y DECOMENTAR EL CODIGO DE ABAJO PARA PROBAR RAPIDAMENTE*/
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingresa las cadenas de adn(ATGC): ");
+        System.out.println("Cada secuencia contiene 6 caracteres, un toal de 6 secuencias");
+        String[] dna = new String[6];
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Secuencia nro" + (i + 1) + ": ");
+            while (true) {
+                String secuencia = s.nextLine().toUpperCase();
+                if (secuencia.matches("^[ATGC]+$")) {
+                    dna[i] = secuencia;
+                    break;
+                } else {
+                    System.out.println("secuencia incorrecto");
+                }
+            }
+        }
+
+        System.out.println("---------------");
+        System.out.println("Es mutante? " + isMutant(dna));
+        System.out.println("---------------");
+
+        /*DESCOMENTAR PARA PROBAR RAPIDAMENTE*/
+
+        /*String[] dnaMutant = {
                 "ATGCAG",
                 "CAGTAC",
                 "CCAAGT",
                 "CGAAGG",
                 "CACCGA",
                 "TCACTG"};
+
+        System.out.println("---------------");
+        System.out.println("Es mutante? " + isMutant(dnaMutant));
+        System.out.println("---------------");
 
         String[] dnaNoMutante = {
                 "ATGCGA",
@@ -86,26 +118,7 @@ public class Main {
                 "TCACTG"};
 
         System.out.println("---------------");
-        System.out.println("ADN MUTANTE");
-        System.out.println("---------------");
-
-        for (String adn : dnaMutant) {
-            System.out.println(adn);
-        }
-
-        System.out.println("---------------");
-        System.out.println("Es mutante? " + isMutant(dnaMutant));
-
-        System.out.println("---------------");
-        System.out.println("ADN NO MUTANTE");
-        System.out.println("---------------");
-
-        for (String adn : dnaMutant) {
-            System.out.println(adn);
-        }
-        System.out.println("---------------");
         System.out.println("Es mutante? " + isMutant(dnaNoMutante));
-        System.out.println("---------------");
+        System.out.println("---------------");*/
     }
-
 }
